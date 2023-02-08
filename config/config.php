4208d92d5858456813ca47dcf017abc812211b8b
ulises12ramos <124833343+ulises12ramos@users.noreply.github.com>
2023-02-08 16:57:29 -0600

    Archivos de configuracion

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+    //carpeto de proyecto
+    define('ROOT_PATH', dirname(__DIR__)."/");
+    define('DATABASE', ROOT_PATH."database/");
+    define('CONTROLLERS', ROOT_PATH."controllers/");
+    define('MODELS', ROOT_PATH."models/");
+    define('REQUESTS', ROOT_PATH."requests/");
+?>
